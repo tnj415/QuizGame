c0566205341dbfe58f22db7b75216ad30fb15f94
Troy Johnson <tnj8510@gmail.com>
2021-06-28 08:28:58 -0700

    took away next button, progress on timer, working on eval function

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,54 +1,54 @@
-var quizContainer = document.querySelector(".quiz-container")
-var preScreen = document.querySelector(".pre-screen")
-var beginGame = document.querySelector(".begin")
-var nxtBtn = document.querySelector(".nxt-btn")
-var results = document.querySelector(".results")
-var opBtn = document.querySelectorAll(".op-btn")
-var askQ = document.querySelector("#question")
-var opA = document.querySelector("#opA")
-var opB = document.querySelector("#opB")
-var opC = document.querySelector("#opC")
-var opD = document.querySelector("#opD")
-var timerEl = document.querySelector("#timer")
+var quizContainer = document.querySelector(".quiz-container");
+var preScreen = document.querySelector(".pre-screen");
+var beginGame = document.querySelector(".begin");
+var results = document.querySelector(".results");
+var opBtn = document.querySelectorAll(".op-btn");
+var askQ = document.querySelector("#question");
+var opA = document.querySelector("#opA");
+var opB = document.querySelector("#opB");
+var opC = document.querySelector("#opC");
+var opD = document.querySelector("#opD");
+var timerEl = document.querySelector("#timer");
 
-beginGame.addEventListener("click", beginQuiz)
+beginGame.addEventListener("click", beginQuiz);
 
-opA.addEventListener("click", evaluateAns)
-opB.addEventListener("click", evaluateAns)
-opC.addEventListener("click", evaluateAns)
-opD.addEventListener("click", evaluateAns)
+opA.addEventListener("click", evaluateAns);
+opB.addEventListener("click", evaluateAns);
+opC.addEventListener("click", evaluateAns);
+opD.addEventListener("click", evaluateAns);
 
-nxtBtn.addEventListener("click", setNextQuestion)
+
 
 var timer = 60;
 var currQ = 0;
 
-function timerFunction(){
+function timerFunction() {
 
-    setInterval(function(){
-        if(timer === 0) {
-            clearInterval(timer = 0)
+    setInterval(function () {
+        if (timer <= 0) {
+            clearInterval(timer = 0);
         }
 
-    timerDisplay.innerHTML=timeLeft
-    timer--
+        timerEl.innerHTML = timer;
+        --timer;
     }, 1000)
 }
 
 function beginQuiz() {
+    timerFunction();
+    preScreen.setAttribute("class", "hide");
+    quizContainer.setAttribute("class", "show");
 
-    preScreen.setAttribute("class", "hide")
-    quizContainer.setAttribute("class", "show")
-    if (timer === 60) timerFunction()
-    setNextQuestion()
+
+    setNextQuestion();
 }
 
 function setNextQuestion() {
 
-   if (currQ > 0) reset()
-    
-    showQuestion(currQ)
-    //evaluateAns();
+    if (currQ > 0) reset();
+
+    showQuestion(currQ);
+    evaluateAns();
 }
 
 function showQuestion(question) {
@@ -60,15 +60,15 @@ function showQuestion(question) {
 
     askQ.innerText = questions[currQ].question;
 
-    opA.innerText = questions[currQ].options[0].text
-    opB.innerText = questions[currQ].options[1].text
-    opC.innerText = questions[currQ].options[2].text
-    opD.innerText = questions[currQ].options[3].text
+    opA.innerText = questions[currQ].options[0].text;
+    opB.innerText = questions[currQ].options[1].text;
+    opC.innerText = questions[currQ].options[2].text;
+    opD.innerText = questions[currQ].options[3].text;
 
-    opA.dataset.correct = questions[currQ].options[0].correct
-    opB.dataset.correct = questions[currQ].options[1].correct
-    opC.dataset.correct = questions[currQ].options[2].correct
-    opD.dataset.correct = questions[currQ].options[3].correct
+    opA.dataset.correct = questions[currQ].options[0].correct;
+    opB.dataset.correct = questions[currQ].options[1].correct;
+    opC.dataset.correct = questions[currQ].options[2].correct;
+    opD.dataset.correct = questions[currQ].options[3].correct;
 
     // console.log(questions[0].options[0].correct)
     // console.log(questions[0].options[1].correct)
@@ -83,98 +83,91 @@ function evaluateAns(e) {
     // console.log(e.target.dataset.correct)
 
     if (e.target.dataset.correct === "true") {
-        opBtn.forEach(el => el.setAttribute("id", "incorrect-ans"))
-        e.target.removeAttribute("id", "incorrect-ans")
-        e.target.setAttribute("id", "correct-ans")
+        opBtn.forEach(el => el.setAttribute("id", "incorrect-ans"));
+        e.target.removeAttribute("id", "incorrect-ans");
+        e.target.setAttribute("id", "correct-ans");
     }
     else {
-        timeLeft -= 5
-        timer.setAttribute("id", "incorectT-effect")
 
         opBtn.forEach((el) => {
             if (el.dataset.correct === "false")
-                el.setAttribute("id", "incorrect-ans")
+                el.setAttribute("id", "incorrect-ans");
             else
-                el.setAttribute("id", "correct-ans")
+                el.setAttribute("id", "correct-ans");
         })
+
+        timer -= 5;
+        timerEl.innerHTML = -5;
+        // timerEl.classList.add("incorrectT-effect")
     }
 
-    currQ++
+    currQ++;
 
-    if (currQ < questions.length) {
-        nxtBtn.classList.remove("hide")
-        nxtBtn.classList.add("show")
-    }
-    else {
-        results.classList.remove("hide")
-        results.classList.add("show")
+    if (currQ === questions.length) {
+  
+        results.classList.remove("hide");
+        results.classList.add("show");
     }
 
-    console.log(nxtBtn)
-    nxtBtn.addEventListener("click", setNextQuestion)
+   setNextQuestion();
 }
 
 function reset() {
 
-
-
     opBtn.forEach((el) => {
 
         if (document.querySelector("#incorrect-ans"))
-        el.removeAttribute("id", "incorrect-ans")
+            el.removeAttribute("id", "incorrect-ans");
 
         else if (document.querySelector("#correct-ans"))
-        el.removeAttribute("id", "incorrect-ans")
+            el.removeAttribute("id", "incorrect-ans");
         else alert("something bad happened here")
     })
 
-    if (nxtBtn.classList.contains("show")) {
-        nxtBtn.classList.remove("show")
-        nxtBtn.classList.add("hide")
-        }
-
-        if (timeEl.classlist.contains(""))
+    if (timerEl.classList.contains("incorrectT-effect")) {
+        timerEl.classList.remove("incorrectT-effect");
+    }
 
 }
 
-    var questions = [
-        {
-            question: "What is the default value of Object variable?",
-            options: [
-                { text: "undefined", correct: false },
-                { text: "0", correct: false },
-                { text: "null", correct: true },
-                { text: "not defined", correct: false },
-            ]
-        },
+var questions = [
+    {
+        question: "What is the default value of Object variable?",
+        options: [
+            { text: "undefined", correct: false },
+            { text: "0", correct: false },
+            { text: "null", correct: true },
+            { text: "not defined", correct: false },
+        ]
+    },
 
-        {
-            question: "What is Abstraction?",
-            options: [
-                { text: "Abstraction is a technique to define different methods of same type", correct: false },
-                { text: "Abstraction is the ability of an object to take on many forms", correct: false },
-                { text: "It refers to the ability to make class abstract in OOP", correct: true },
-                { text: "None of the above", correct: false },
-            ]
-        },
+    {
+        question: "What is Abstraction?",
+        options: [
+            { text: "Abstraction is a technique to define different methods of same type", correct: false },
+            { text: "Abstraction is the ability of an object to take on many forms", correct: false },
+            { text: "It refers to the ability to make class abstract in OOP", correct: true },
+            { text: "None of the above", correct: false },
+        ]
+    },
 
-        {
-            question: "What is the default value of Object variable?",
-            options: [
-                { text: "undefined", correct: false },
-                { text: "0", correct: false },
-                { text: "null", correct: true },
-                { text: "not defined", correct: false },
-            ]
-        },
+    {
+        question: "What is the default value of Object variable?",
+        options: [
+            { text: "undefined", correct: false },
+            { text: "0", correct: false },
+            { text: "null", correct: true },
+            { text: "not defined", correct: false },
+        ]
+    },
 
-        {
-            question: "What is the default value of Object variable?",
-            options: [
-                { text: "undefined", correct: false },
-                { text: "0", correct: false },
-                { text: "null", correct: true },
-                { text: "not defined", correct: false },
-            ]
-        }
-    ]
+    {
+        question: "What is the default value of Object variable?",
+        options: [
+            { text: "undefined", correct: false },
+            { text: "0", correct: false },
+            { text: "null", correct: true },
+            { text: "not defined", correct: false },
+        ]
+    }
+]
