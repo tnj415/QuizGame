056b633a2bf97ba52880261abcf632cc9ae208d2
Troy Johnson <tnj8510@gmail.com>
2021-06-27 21:57:51 -0700

    completed evaluate function

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,14 +1,15 @@
-var quizContainer = document.querySelector(".quiz-container");
-var preScreen = document.querySelector(".pre-screen");
-var beginGame = document.querySelector(".begin");
+var quizContainer = document.querySelector(".quiz-container")
+var preScreen = document.querySelector(".pre-screen")
+var beginGame = document.querySelector(".begin")
+var nxtBtn = document.querySelector(".nxt-btn")
 
-var opBtn = document.querySelectorAll("op-btn");
+var opBtn = document.querySelectorAll(".op-btn")
 
-var askQ = document.querySelector("#question");
-var opA = document.querySelector("#opA");
-var opB = document.querySelector("#opB");
-var opC = document.querySelector("#opC");
-var opD = document.querySelector("#opD");
+var askQ = document.querySelector("#question")
+var opA = document.querySelector("#opA")
+var opB = document.querySelector("#opB")
+var opC = document.querySelector("#opC")
+var opD = document.querySelector("#opD")
 
 beginGame.addEventListener("click", beginQuiz)
 
@@ -17,88 +18,122 @@ opB.addEventListener("click", evaluateAns)
 opC.addEventListener("click", evaluateAns)
 opD.addEventListener("click", evaluateAns)
 
+nxtBtn.addEventListener("click", setNextQuestion)
+
+var currQ = 0;
 
 function beginQuiz() {
 
-    preScreen.setAttribute("class", "hide");
-    quizContainer.setAttribute("class", "show");
+    preScreen.setAttribute("class", "hide")
+    quizContainer.setAttribute("class", "show")
     setNextQuestion()
 }
 
+
+
 function setNextQuestion() {
-    showQuestion();
-    evaluateAns();
+
+    if (nxtBtn.classList === "show") {
+    nxtBtn.classList.remove("show")
+    nxtBtn.classList.add("hide")
+    }
+    
+    showQuestion(currQ);
+    //evaluateAns();
 }
 
 function showQuestion(question) {
 
-    askQ.innerText = questions[0].question;
+    //     console.log(questions) //should print the whole questions array
+    //   console.log(questions[0]) //this should be the first element of that array... which is an object.
+    //   console.log(questions[0].question) //see how we're working our way through the array's objects?
+    //   console.log(questions[0].options[1]) //what do you think this will store? console.log(myVar) to find out!
 
-    opA.innerText = questions[0].options[0].text
-    opB.innerText = questions[0].options[1].text
-    opC.innerText = questions[0].options[2].text
-    opD.innerText = questions[0].options[3].text
+    askQ.innerText = questions[currentQuestion].question;
 
-    opA.dataset = questions[0].options[0].correct
-    opB.dataset = questions[0].options[1].correct
-    opC.dataset = questions[0].options[2].correct
-    opD.dataset = questions[0].options[3].correct
+    opA.innerText = questions[currQ].options[0].text
+    opB.innerText = questions[currQ].options[1].text
+    opC.innerText = questions[currQ].options[2].text
+    opD.innerText = questions[currQ].options[3].text
 
-    console.log(questions[0].options[0].correct)
-    console.log(questions[0].options[1].correct)
-    console.log(questions[0].options[2].correct)
-    console.log(questions[0].options[3].correct)
+    opA.dataset.correct = questions[currQ].options[0].correct
+    opB.dataset.correct = questions[currQ].options[1].correct
+    opC.dataset.correct = questions[currQ].options[2].correct
+    opD.dataset.correct = questions[currQ].options[3].correct
 
-}
-
-function evaluateAns(element) {
-
-//     if (this.dataset === correct) {
-//         opBtn.setAttribute("class", "incorrect-ans")
-//         this.removeAttribute("class", "incorrect-ans")
-//         this.setAttribute("class", "correct-ans")
-//    }
+    // console.log(questions[0].options[0].correct)
+    // console.log(questions[0].options[1].correct)
+    // console.log(questions[0].options[2].correct)
+    // console.log(questions[0].options[3].correct)
 
 }
 
-var questions = [
-    {
-        question: "What is the default value of Object variable?",
-        options: [
-            { text: "undefined", correct: false },
-            { text: "0", correct: false },
-            { text: "null", correct: true },
-            { text: "not defined", correct: false },
-        ]
-    },
+function evaluateAns(e) {
 
-    {
-        question: "What is Abstraction?",
-        options: [
-            { text: "Abstraction is a technique to define different methods of same type", correct: false },
-            { text: "Abstraction is the ability of an object to take on many forms", correct: false },
-            { text: "It refers to the ability to make class abstract in OOP", correct: true },
-            { text: "None of the above", correct: false },
-        ]
-    },
+    // console.log(e.target)
+    // console.log(e.target.dataset.correct)
 
-    {
-        question: "What is the default value of Object variable?",
-        options: [
-            { text: "undefined", correct: false },
-            { text: "0", correct: false },
-            { text: "null", correct: true },
-            { text: "not defined", correct: false },
-        ]
-    },
-
-    {
-        question: "What is the default value of Object variable?",
-        options: [
-            { text: "undefined", correct: false },
-            { text: "0", correct: false },
-            { text: "null", correct: true },
-            { text: "not defined", correct: false },
-        ]
+    if (e.target.dataset.correct === "true") {
+        opBtn.forEach(el => el.setAttribute("id", "incorrect-ans"))
+        e.target.removeAttribute("id", "incorrect-ans")
+        e.target.setAttribute("id", "correct-ans")
     }
-]
+    else {
+        opBtn.forEach((el) => {
+            if (el.dataset.correct === "false")
+                el.setAttribute("id", "incorrect-ans")
+            else
+                el.setAttribute("id", "correct-ans")
+        })
+    }
+
+    if (currQ < questions.length) {
+        nxtBtn.classList.remove("hide")
+        nxtBtn.classList.add("show")
+    }
+    else {
+        resltBtn.classList.remove("hide")
+        resltBtn.classList.add("show")
+    }
+
+    var questions = [
+        {
+            question: "What is the default value of Object variable?",
+            options: [
+                { text: "undefined", correct: false },
+                { text: "0", correct: false },
+                { text: "null", correct: true },
+                { text: "not defined", correct: false },
+            ]
+        },
+
+        {
+            question: "What is Abstraction?",
+            options: [
+                { text: "Abstraction is a technique to define different methods of same type", correct: false },
+                { text: "Abstraction is the ability of an object to take on many forms", correct: false },
+                { text: "It refers to the ability to make class abstract in OOP", correct: true },
+                { text: "None of the above", correct: false },
+            ]
+        },
+
+        {
+            question: "What is the default value of Object variable?",
+            options: [
+                { text: "undefined", correct: false },
+                { text: "0", correct: false },
+                { text: "null", correct: true },
+                { text: "not defined", correct: false },
+            ]
+        },
+
+        {
+            question: "What is the default value of Object variable?",
+            options: [
+                { text: "undefined", correct: false },
+                { text: "0", correct: false },
+                { text: "null", correct: true },
+                { text: "not defined", correct: false },
+            ]
+        }
+    ]
