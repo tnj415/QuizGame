52159f31abef9203ecaa16c9dc9deca40119d6bb
Troy Johnson <tnj8510@gmail.com>
2021-06-27 17:24:34 -0700

    updating js.js

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,9 +1,38 @@
-var startQuiz = Quiz.querySelector(".start-quiz");
+var quizContainer = document.querySelector(".quiz-container");
+var preScreen = document.querySelector(".pre-screen");
+var beginGame = document.querySelector(".begin");
+var questionElement = document.querySelector('#question');
+var answerBtnElement = document.querySelectorAll('.option-btn');
 
-var selectAnswerA = Quiz.querySelector("answerA");
-var selectAnswerB = Quiz.querySelector("answerB");
-var selectAnswerC = Quiz.querySelector("answerC");
-var selectAnswerD = Quiz.querySelector("answerD");
 
-var quizContainer = Quiz.querySelector("quiz-container");
-quizContainer.classList.add(".hide");
+
+beginGame.addEventListener("click", beginQuiz)
+
+function beginQuiz() {
+
+    preScreen.setAttribute("class", "hide");
+    quizContainer.setAttribute("class", "show");
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+    showQuestion();
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+}
+
+
+
+var questions = [
+    {
+        question: "What is 2 + 2",
+        options: [
+            { text: "2", correct: false },
+            { text: "4", correct: true },
+            { text: "6", correct: false },
+            { text: "8", correct: false },
+        ]
+    }
+]
