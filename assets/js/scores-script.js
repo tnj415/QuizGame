d23514df8d11869ecd5cd88b211f68825762d5c9
Troy Johnson <tnj8510@gmail.com>
2021-06-30 21:15:13 -0700

    fixing 2d array

--- assets/js/scores-script.js
+++ assets/js/scores-script.js
@@ -7,6 +7,7 @@ var leaderBoardBtn = document.querySelector("#enter-leader-board");
 var score = JSON.parse(localStorage.getItem("scoreLog"));
 var gameType = JSON.parse(localStorage.getItem("gameType"));
 var userInput = "";
+var memberNameStored = userInput;
 var memberCountEl = document.querySelector("#member-count");
 var memberNameEl = document.querySelector("#member-name");
 var memberScoreEl = document.querySelector("#member-score");
@@ -14,27 +15,36 @@ var memberScoreEl = document.querySelector("#member-score");
 submitEl.addEventListener("click", showResponse);
 leaderBoardBtn.addEventListener("click", enterLeaderBoard);
 
-var jpArrTimed = (JSON.parse(localStorage.getItem("storedBoardTimed")).split(' '))
-var jpArrUntimed = (JSON.parse(localStorage.getItem("storedBoardUntimed")).split(' '))
+var jpArrTimed = [];
+var jpArrUntimed = [];
 // var leaderBoardArr = [
 //     {memberName = userInput},
 //     {memberScore = localStorage.getItem("scoreLog")},
 //     {memberGameType = localStorage.getItem("gameType")}
 // ]
-var storedBoardObjTimed = JSON.parse(localStorage.getItem(storedBoardTimed));
-var storedBoardObjUntimed = JSON.parse(localStorage.getItem(storedBoardUntimed));
+var storedBoardObjTimed = JSON.parse(localStorage.getItem("storedBoardTimed"));
+var storedBoardObjUntimed = JSON.parse(localStorage.getItem("storedBoardUntimed"));
 var leaderBoardArrTimed = [];
 var leaderBoardArrUntimed = [];
 
 function enterLeaderBoard() {
     event.preventDefault();
+    setLeaderBoard();
     $(".score-card").addClass("hide");
     $(".leader-board").removeClass("hide");
     $(".leader-board").addClass("show");
+
 }
-function getLeaderBoard() {
-    if (storedBoard.length === 0) memberListCount = 0;
-    else memberListCount = storedBoard.length;
+function getTimedLeaderBoard() {
+    if (storedBoardObjTimed.length === 0) memberListCount = 0;
+    else memberListCount = storedBoardObjTimed.length;
+
+    memberCountEl.textContent = memberListCount;
+}
+
+function getUntimedLeaderBoard() {
+    if (storedBoardObjUntimed.length === 0) memberListCount = 0;
+    else memberListCount = storedBoardObjUntimed.length;
 
     memberCountEl.textContent = memberListCount;
 }
@@ -42,45 +52,63 @@ function getLeaderBoard() {
 function setLeaderBoard() {
     setGameType();
     setScore();
+
+    jpArrTimed.push(userInput);
+    jpArrTimed.push(parseInt(score, 10) / 100);
+
+    if (localStorage.getItem("storedBoardTimed") !== null)
+        jpArrTimed = (JSON.parse(localStorage.getItem("storedBoardTimed")).split(' '));
+    if (localStorage.getItem("storedBoardUntimed") !== null)
+        jpArrUntimed = (JSON.parse(localStorage.getItem("storedBoardUntimed")).split(' '));
+    //     if (gameType)
+    //     showTimedLeaderBoard();
+    // else if (!gameType)
+    console.log(jpArrTimed)
     showTimedLeaderBoard();
+    //else alert("ERROR: in setLeaderBoard if statement");
 
     // memberCountEl.textContent = memberListCount;
     // localStorage.setItem("storedBoardObj", storedBoard);
 
     // memberNameEl.textContent = memberName;
     // localStorage.setItem("storedBoardObj", storedBoard);
-
-    // memberScoreEl.textContent = memberScore;
-    // localStorage.setItem("storedBoardObj", storedBoard);
+    console.log(leaderBoardArrTimed)
 }
 
 function showTimedLeaderBoard() {
 
-    for (var i = 0; i < jpArr.length / 2; i += 2) {
-        leaderBoardArrTimed[i].push([jpArr[i], jpArr[i + 1]])
+    console.log("jpArrTimed.length = " + jpArrTimed.length)
+    for (var i = 0; i < jpArrTimed.length / 2; i += 2) {
+        
+        console.table("leaderBoardArrTimed = " + leaderBoardArrTimed);
+        console.log("leaderBoardArrTimed[i] = " + leaderBoardArrTimed[i]);
+        
+        leaderBoardArrTimed.push([jpArrTimed[i], jpArrTimed[i + 1]])
 
+        console.table("leaderBoardArrTimed = " + leaderBoardArrTimed);
+        console.log("leaderBoardArrTimed[i] = " + leaderBoardArrTimed[i]);
     }
 
-        leaderBoardArrTimed.forEach((el, i) => {
-        $("<ol>").append("<li>" + leaderBoardArrTimed[i][el] + " - " + leaderBoardArrTimed[i][el])
-        memberListCount++
+    leaderBoardArrTimed.forEach((el, i) => {
+        $(".rank").append("<li>" + leaderBoardArrTimed[i][el] + " - " + leaderBoardArrTimed[i][el])
+        //memberListCount++
         // memberCountEl.textContent = leaderBoardArr.length;
         // memberNameEl.textContent = leaderBoardArr[i][el];
         // memberScoreEl.textContent = memberScore[i][el];
     })
-    if (memberListCount !== leaderBoardArrTimed.length) alert("mlc != lbArr")
+    //if (memberListCount !== leaderBoardArrTimed.length) alert("mlc != lbArr")
 
 }
 function showUntimedLeaderBoard() {
 
-    for (var i = 0; i < jpArr.length / 2; i += 2) {
-        leaderBoardArr[i].push([jpArr[i], jpArr[i + 1]])
+    for (var i = 0; i < jpArrUntimed.length / 2; i += 2) {
+        leaderBoardArrUntimed[i].push([jpArrUntimed[i], jpArrUntimed[i + 1]]);
 
     }
 
 
     leaderBoardArrUntimed.forEach((el, i) => {
-        $("<ol>").append("<li>" + leaderBoardArrUntimed[i][el] + " - " + leaderBoardArrUnt[i][el])
+        $(".rank").append("<li>" + leaderBoardArrUntimed[i][el] + " - " + leaderBoardArrUntimed[i][el])
         memberListCount++
         // memberCountEl.textContent = leaderBoardArr.length;
         // memberNameEl.textContent = leaderBoardArr[i][el];
@@ -91,14 +119,13 @@ function showUntimedLeaderBoard() {
 }
 
 function init() {
-    getLeaderBoard();
+    
+    
 }
 
 function setGameType() {
-    if (gameType)
-        setGameTypeEl.textContent = "Game Type: Timed";
-    else if (!gameType)
-        setGameTypeEl.textContent = "Game Type: Un-Timed";
+    if (gameType) setGameTypeEl.textContent = "Game Type: Timed";
+    else if (!gameType) setGameTypeEl.textContent = "Game Type: Un-Timed";
     else alert("problem setting Game Type");
 }
 
@@ -106,13 +133,14 @@ function setScore() {
     if (gameType) setScoreEl.textContent = parseInt(score, 10) / 100;
     else if (!gameType) setScoreEl.textContent = score;
     else alert("problem setting score");
-    setLeaderBoard();
+    
 }
 
 function showResponse(e) {
     e.preventDefault();
     if (memberEl.value != "") {
         var userInput = memberEl.value;
+        //localStorage.setItem("memberNameStored", userInput)
         var response = userInput + " is a contender!";
         document.querySelector("#response").textContent = response.toUpperCase()
 
